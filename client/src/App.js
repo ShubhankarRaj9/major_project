3573bd7c9115d7e59081760ec18e5b57222e2bf2
ShubhankarRaj9 <sparshrajsr@gmail.com>
2026-04-09 20:53:19 +0530

    Initial commit - chatbot project

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import EcommerceStore from './components/EcommerceStore'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <EcommerceStore/>
+    </div>
+  )
+}
+
+export default App
